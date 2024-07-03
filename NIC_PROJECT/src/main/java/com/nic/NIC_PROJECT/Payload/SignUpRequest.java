a275f1176f015d2d4b1bb743914deea56aebf7b2
Arka Bhattacharjee <kailashahar123@gmail.com>
2024-07-03 15:14:36 +0530

    Resolve merge conflicts

---
+++ NIC_PROJECT/src/main/java/com/nic/NIC_PROJECT/Payload/SignUpRequest.java
@@ -0,0 +1,17 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class SignUpRequest {
+
+    private long mobileNo;
+    private String emailId;
+    private String name;
+    private String gender;
+    private String dob;
+    private String address;
+}
